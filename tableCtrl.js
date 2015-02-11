d6696ede6188ac2e92ec190c45da23555e7d2fe4
Mark Tonkinson <DHTMaster@gmail.com>
2015-02-11 15:16:46 -0700

    Getting close to having finished product

--- tableCtrl.js
+++ tableCtrl.js
@@ -1,69 +1,100 @@
 var app = angular.module("NeedleTable");
 
 app.controller("tableCtrl", function($scope, tableService){
-	
+	//to do, need to check if my inputs are dirty so it doesn't just keep adding up or multplying
 	$scope.table = {
 		colHeads: tableService.getColumnHeadings(),
 		rows: [],
-		row1: [],
-		totalRow: ['0','0','0','0','0','0','0'],
-		equations: {
-			multiply: function(val, colIndex){
-				if(typeof val === "string"){
-					//parseFloat because they give decimals
-					val = parseFloat(val)
-				}
-				if(typeof colIndex === "string"){
-					colIndex = parseInt(colIndex)
-				}
-				return val * colIndex
+		columnTotals: [],
+		multiply: function(val, index, cb){
+			if(typeof val === "string"){
+				//parseFloat because they give decimals
+				val = parseFloat(val)
 			}
-		},
-		rowCounter: function(){
-			var counter = 1;
-			return function(){
-				counter++
-				return counter;
-				
+			if(typeof index === "string"){
+				index = parseInt(index)
 			}
+			return val * index
 		},
-		rowCounterPlus: function(){
-			var runCounter = this.rowCounter()
-			return runCounter()
+		sumRow: function(row, oldVal, newVal){
+			var total = row[6]
+			oldVal = parseFloat(oldVal)
+			newVal = parseFloat(newVal)
+			total.total(oldVal, newVal);
+			var num = parseFloat(total.val);
+			if(total.addRowFlag === true && num > 0){
+				this.formNewRow();
+				total.addRowFlag = false;
+			}
+			console.log($scope.table.rows)
 		},
-		addRow: function(){
-
+		sumColumn: function(columnRef, oldVal, newVal){
+			
+			var i = columnRef.replace('col', '')
+			i = parseFloat(i)
+			i = i-1;
+			var total = this.columnTotals[i][columnRef]
+			total = parseFloat(total);
+			total -= oldVal;
+			total += newVal;
+			this.columnTotals[i][columnRef] = total.toString();
+			console.log(columnRef, i, total)
 		},
 		formNewRow: function(){
-			var row;
-			if(this.row1.length === 0){
-				row = 'row1'
-			} else {
-				row = 'row' + this.rowCounterPlus();
-				this[row] = [];			
-			}
-			for(var i=0; i<this.colHeads.length; i++){
-				var ref = "col" + i;
+			var row = []
+			for(var i=0; i<this.colHeads.length-1; i++){
+				var ref="col"+ (i+1);
 				var obj = {
-					ref: i,
+					columnRef: ref,
+					index: i + 1,
 					val: '0',
-					equation: function(){
-						this.val = equations.multiply(val, ref);
+					valRef: '0',
+					equation: function(row){
+						if(this.val === ''){
+							this.val = '0'
+						} else if (this.val !== this.valRef){
+							//using "this.equations" didn't go to the parent prototype- how do I do that? also, because I'm not using a constructor this is creating this function again and again
+							//I think this would need to be a constructor to share the parent prototype? Need to research
+							this.val = $scope.table.multiply(this.val, this.index);
+							$scope.table.sumRow(row, this.valRef, this.val);
+							$scope.table.sumColumn(this.columnRef, this.valRef, this.val)
+							this.valRef = this.val;
+						}
 					}
 				}
-				this[row].push(obj)
-
+				row.push(obj)
 			}
-			this.rows.push(this[row])
-		},
-		sumRow: function(){
-			this.formNewRow();
-			console.log(this)
-
+			row.push({
+				addRowFlag: true,
+				val: '0',
+				total: function(oldVal, newVal){
+					this.val = parseFloat(this.val);
+					this.val -= oldVal;
+					this.val += newVal;
+					this.val = this.val.toString()
+				}
+			})
+			this.rows.push(row)
 		}
 
-	}
+	};
 
-	$scope.table.sumRow()
-	$scope.table.sumRow();
+	
+	var initTable = function(){
+		if($scope.table.rows.length === 0){
+			$scope.table.formNewRow();
+		}
+		
+		for(var i=1; i<7; i++){
+			var obj = {};
+			var key = 'col' + i
+			obj[key] = '0'
+			$scope.table.columnTotals.push(obj)
+		}
+		console.log($scope.table.columnTotals)
+	};
+	initTable()
+	
+	
+
 });
